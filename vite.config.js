f5a7d1d4c1bed5fe5b3973c1a2ae7c6befd49699
d4niel-olaya <olayajuandaniel8@gmail.com>
2025-04-24 19:30:58 -0500

    configuracion vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+function getJsInputs(directory) {
+    const dirPath = resolve(__dirname, directory);
+    return readdirSync(dirPath)
+        .filter(file => file.endsWith('.js')) // Filtrar solo archivos .js
+        .map(file => `${directory}/${file}`); // Crear la ruta relativa
+}
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css',  ...getJsInputs('resources/js') ],
             refresh: true,
         }),
     ],
